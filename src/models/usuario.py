fdaf2593f029ebcde36b793180369572b5578390
Rsapa604 <markis01pro@gmail.com>
2025-05-15 18:25:35 +0200

    usuario2

--- src/models/usuario.py
+++ src/models/usuario.py
@@ -1,5 +1,9 @@
+import uuid
+from typing import List
+
 class User:
-    def __init__(self, username, password_hash):
+    def __init__(self, username: str, password_hash: str):
+        self.id = uuid.uuid4()
         self.username = username
         self.password_hash = password_hash
         self.tokens = []
