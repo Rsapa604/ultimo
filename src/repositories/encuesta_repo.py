9bee62f3f6997eb16a4f66387a1e7413a481684d
Rsapa604 <markis01pro@gmail.com>
2025-05-15 17:56:12 +0200

    enc_repo2

--- src/repositories/encuesta_repo.py
+++ src/repositories/encuesta_repo.py
@@ -14,3 +14,17 @@ class EncuestaRepository:
         encuestas = [e for e in encuestas if e['id'] != str(encuesta.id)]
         encuestas.append(self._serialize(encuesta))
         self.filepath.write_text(json.dumps(encuestas, indent=2))
+
+    def buscar_por_id(self, poll_id: UUID):
+        encuestas = self._cargar_todos()
+        for e in encuestas:
+            if e['id'] == str(poll_id):
+                return self._deserialize(e)
+        return None
+
+    def listar_todos(self):
+        encuestas = self._cargar_todos()
+        return [self._deserialize(e) for e in encuestas]
+
+    def _cargar_todos(self):
+        return json.loads(self.filepath.read_text())
