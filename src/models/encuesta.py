33f96fbca4f26d0d384d453404ea1926591c40a1
Rsapa604 <markis01pro@gmail.com>
2025-05-15 17:53:08 +0200

    encuesta2

--- src/models/encuesta.py
+++ src/models/encuesta.py
@@ -17,3 +17,24 @@ class Poll:
         self.timestamp_inicio = datetime.now()
         self.duracion = timedelta(seconds=duracion_segundos)
         self.votantes = set()
+
+    def esta_activa(self):
+        if self.estado == PollState.CLOSED:
+            return False
+        if datetime.now() > self.timestamp_inicio + self.duracion:
+            self.estado = PollState.CLOSED
+            return False
+        return True
+
+    def agregar_voto(self, username, opcion):
+        if not self.esta_activa():
+            raise Exception("Encuesta cerrada")
+        if username in self.votantes:
+            raise Exception("Usuario ya votó")
+        if opcion not in self.opciones:
+            raise Exception("Opción inválida")
+        self.votos[opcion] += 1
+        self.votantes.add(username)
+
+    def cerrar(self):
+        self.estado = PollState.CLOSED
