4e601a1b6ddc6e57b6eaca858f81d9978b22e737
Rsapa604 <markis01pro@gmail.com>
2025-05-15 17:57:52 +0200

    usu_repo2

--- src/repositories/usuario_repo.py
+++ src/repositories/usuario_repo.py
@@ -13,3 +13,20 @@ class UsuarioRepository:
         usuarios = [u for u in usuarios if u['username'] != user.username]
         usuarios.append(self._serialize(user))
         self.filepath.write_text(json.dumps(usuarios, indent=2))
+
+    def buscar_por_username(self, username):
+        usuarios = self._cargar_todos()
+        for u in usuarios:
+            if u['username'] == username:
+                return self._deserialize(u)
+        return None
+
+    def _cargar_todos(self):
+        return json.loads(self.filepath.read_text())
+
+    def _serialize(self, user: User):
+        return {
+            "username": user.username,
+            "password_hash": user.password_hash,
+            "tokens": user.tokens
+        }
